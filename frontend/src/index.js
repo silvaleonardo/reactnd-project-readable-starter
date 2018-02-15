411c9f7098ec4ee193ae4a95599a30b3ab5cadd3
Leonardo Silva <s.leosoares@gmail.com>
2018-02-15 13:48:04 -0200

    feature: Added store, reducers combine and initial config from redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
 import registerServiceWorker from './registerServiceWorker';
+import store from './app/store';
+import './index.css';
+import App from './app/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={ store }>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
