1b443dbb3d2c4edb0361e6e17897ac6c545ba544
Leonardo Silva <s.leosoares@gmail.com>
2018-02-15 13:54:36 -0200

    feature: Added router and config

--- frontend/src/app/App.js
+++ frontend/src/app/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import Routes from './Routes';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Welcome to react!</h1>
+        <header>
+          <h1>Welcome to react!</h1>
+        </header>
+        <main>
+          <Routes />
+        </main>
       </div>
     );
   }
