57e7d39e49bdab93cece6a7d5d559767e4d72b03
Leonardo Silva <s.leosoares@gmail.com>
2018-03-12 18:28:35 -0300

    feature: Created component Header

---
+++ frontend/src/commons/Header.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import IconButton from 'material-ui/IconButton';
+import Typography from 'material-ui/Typography';
+import MenuIcon from 'material-ui-icons/Menu';
+
+const Header = ({ menuToggleClick }) => (
+  <AppBar
+    className="app-header"
+    position="static"
+  >
+    <Toolbar>
+      <IconButton
+        color="inherit"
+        aria-label="Menu"
+        onClick={ () => menuToggleClick() }
+      >
+        <MenuIcon />
+      </IconButton>
+
+      <Typography
+        className="app-header__title"
+        variant="title"
+        color="inherit"
+      >
+        Readable
+      </Typography>
+    </Toolbar>
+  </AppBar>
+);
+
+Header.propTypes = {
+  menuToggleClick: PropTypes.func.isRequired
+};
+
+export default Header;
