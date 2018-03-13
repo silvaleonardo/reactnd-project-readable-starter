df6dd77c7e686e79c7ca0838f6560a6f695a29c6
Leonardo Silva <s.leosoares@gmail.com>
2018-03-13 11:30:58 -0300

    refactor: Changed component PostCard to stateless

--- frontend/src/PostsList/components/PostCard.js
+++ frontend/src/PostsList/components/PostCard.js
@@ -16,93 +16,87 @@ import ThumbDownIcon from 'material-ui-icons/ThumbDown';
 import EditIcon from 'material-ui-icons/Edit';
 import DeleteIcon from 'material-ui-icons/Delete';
 
-class PostCard extends Component {
-  static propTypes = {
-    data: PropTypes.shape({
-      id: PropTypes.string.isRequired,
-      timestamp: PropTypes.number.isRequired,
-      title: PropTypes.string.isRequired,
-      author: PropTypes.string.isRequired,
-      voteScore: PropTypes.number.isRequired,
-      commentCount: PropTypes.number.isRequired
-    }).isRequired,
-    onVoteScore: PropTypes.func.isRequired,
-    onEdit: PropTypes.func.isRequired,
-    onDelete: PropTypes.func.isRequired
-  };
+const PostCard = ({ data, onVoteScore, onEdit, onDelete }) => (
+  <Card className="post-card">
+    <CardHeader
+      avatar={ <Avatar>{ data.title.charAt(0).toUpperCase() }</Avatar> }
+      title={ data.title }
+      subheader={ `${moment(data.timestamp).format('DD/MM/YYYY')} | ${data.author}` }
+    />
 
-  render() {
-    const { data, onVoteScore, onEdit, onDelete } = this.props;
+    <Divider />
 
-    return (
-      <Card className="post-card">
-        <CardHeader
-          avatar={ <Avatar>{ data.title.charAt(0).toUpperCase() }</Avatar> }
-          title={ data.title }
-          subheader={ `${moment(data.timestamp).format('DD/MM/YYYY')} | ${data.author}` }
-        />
+    <CardContent>
+      <Grid container>
+        <Grid item xs className="post-card__grid-item">
+          <Chip
+            className="post-card__chip"
+            avatar={ <Avatar><StarIcon /></Avatar> }
+            label={ data.voteScore }
+          />
+        </Grid>
 
-        <Divider />
+        <Grid item xs className="post-card__grid-item">
+          <Chip
+            className="post-card__chip"
+            avatar={ <Avatar><CommentIcon /></Avatar> }
+            label={ data.commentCount }
+          />
+        </Grid>
+      </Grid>
+    </CardContent>
 
-        <CardContent>
-          <Grid container>
-            <Grid item xs className="post-card__grid-item">
-              <Chip
-                className="post-card__chip"
-                avatar={ <Avatar><StarIcon /></Avatar> }
-                label={ data.voteScore }
-              />
-            </Grid>
+    <CardActions>
+      <Grid container>
+        <Grid item xs className="post-card__grid-item">
+          <IconButton
+            className="post-card__btn-action"
+            onClick={ () => onVoteScore(data.id, 'upVote') }
+          >
+            <ThumbUpIcon />
+          </IconButton>
+        </Grid>
+        <Grid item xs className="post-card__grid-item">
+          <IconButton
+            className="post-card__btn-action"
+            onClick={ () => onVoteScore(data.id, 'downVote') }
+          >
+            <ThumbDownIcon />
+          </IconButton>
+        </Grid>
+        <Grid item xs className="post-card__grid-item">
+          <IconButton
+            className="post-card__btn-action"
+            onClick={ () => onEdit(data.id) }
+          >
+            <EditIcon />
+          </IconButton>
+        </Grid>
+        <Grid item xs className="post-card__grid-item">
+          <IconButton
+            className="post-card__btn-action"
+            onClick={ () => onDelete(data.id) }
+          >
+            <DeleteIcon />
+          </IconButton>
+        </Grid>
+      </Grid>
+    </CardActions>
+  </Card>
+);
 
-            <Grid item xs className="post-card__grid-item">
-              <Chip
-                className="post-card__chip"
-                avatar={ <Avatar><CommentIcon /></Avatar> }
-                label={ data.commentCount }
-              />
-            </Grid>
-          </Grid>
-        </CardContent>
-
-        <CardActions>
-          <Grid container>
-            <Grid item xs className="post-card__grid-item">
-              <IconButton
-                className="post-card__btn-action"
-                onClick={ () => onVoteScore('upVote') }
-              >
-                <ThumbUpIcon />
-              </IconButton>
-            </Grid>
-            <Grid item xs className="post-card__grid-item">
-              <IconButton
-                className="post-card__btn-action"
-                onClick={ () => onVoteScore('downVote') }
-              >
-                <ThumbDownIcon />
-              </IconButton>
-            </Grid>
-            <Grid item xs className="post-card__grid-item">
-              <IconButton
-                className="post-card__btn-action"
-                onClick={ () => onEdit(data.id) }
-              >
-                <EditIcon />
-              </IconButton>
-            </Grid>
-            <Grid item xs className="post-card__grid-item">
-              <IconButton
-                className="post-card__btn-action"
-                onClick={ () => onDelete(data.id) }
-              >
-                <DeleteIcon />
-              </IconButton>
-            </Grid>
-          </Grid>
-        </CardActions>
-      </Card>
-    );
-  };
-}
+PostCard.propTypes = {
+  data: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    timestamp: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
+    voteScore: PropTypes.number.isRequired,
+    commentCount: PropTypes.number.isRequired
+  }).isRequired,
+  onVoteScore: PropTypes.func.isRequired,
+  onEdit: PropTypes.func.isRequired,
+  onDelete: PropTypes.func.isRequired
+};
 
 export default PostCard;
