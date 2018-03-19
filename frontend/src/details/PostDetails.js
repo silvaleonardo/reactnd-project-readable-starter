d2d583bcd7c1654bd2ccb5df95c5ae3aacc6186f
Leonardo Silva <s.leosoares@gmail.com>
2018-03-19 11:45:18 -0300

    feature: Layout of post details

--- frontend/src/details/PostDetails.js
+++ frontend/src/details/PostDetails.js
@@ -1,11 +1,63 @@
 import React, { Component } from 'react';
+import moment from 'moment';
+
+import Grid from 'material-ui/Grid';
+import Paper from 'material-ui/Paper';
+import Typography from 'material-ui/Typography';
+
+import PostDetailsActions from './PostDetailsActions';
 
 class PostDetails extends Component {
   render() {
     return (
-      <section className="post-details">
-        <h1>Post Details</h1>
-      </section>
+      <Paper
+        className="post-details"
+        component="section"
+      >
+        <Grid
+          className="post-details__header"
+          component="header"
+          container
+        >
+          <Grid item xs={8}>
+            <Typography
+              className="post-details__header-title"
+              variant="title"
+            >
+              Udacity is the best place to learn React <small>(React)</small>
+            </Typography>
+
+            <Typography
+              className="post-details__header-subtitle"
+              variant="subheading"
+            >
+              { `${moment(1467166872634).format('DD/MM/YYYY')} | thingtwo` }
+            </Typography>
+
+            <Typography
+              className="post-details__header-subtitle"
+              variant="caption"
+            >
+              Votes: 6 | Comments: 2
+            </Typography>
+          </Grid>
+
+          <Grid
+            item
+            xs={4}
+            component={ PostDetailsActions }
+            editLink={ `/react/1/edit` }
+            onVoteScore={ () => {} }
+            onDelete={ () => {} }
+          />
+        </Grid>
+
+        <section className="post-details__content">
+          <Typography paragraph>
+            Everyone says so after all.
+          </Typography>
+        </section>
+      </Paper>
     );
   }
 }
