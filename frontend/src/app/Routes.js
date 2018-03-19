fb8b74875e090254fe65914870608a6e33bc72f1
Leonardo Silva <s.leosoares@gmail.com>
2018-03-19 10:32:46 -0300

    feature: Created component PostDetails and route for details

--- frontend/src/app/Routes.js
+++ frontend/src/app/Routes.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
+import PostDetails from '../details/PostDetails';
 import PostList from '../list/PostList';
 
 export default () => (
   <Switch>
     <Route exact path="/" component={ PostList } />
     <Route exact path="/:category" component={ props => <PostList { ...props } /> } />
+
+    <Route exact path="/:category/:postId" component={ PostDetails } />
 
     <Redirect from="*" to="/" />
   </Switch>
---
+++ frontend/src/details/PostDetails.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PostDetails extends Component {
+  render() {
+    return (
+      <section className="post-details">
+        <h1>Post Details</h1>
+      </section>
+    );
+  }
+}
+
+export default PostDetails;
