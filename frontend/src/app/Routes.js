3d99463084318b8f9d31ae702968828716092327
Leonardo Silva <s.leosoares@gmail.com>
2018-07-01 20:40:06 -0300

    feature: Added rerender method in React Router

--- frontend/src/app/Routes.js
+++ frontend/src/app/Routes.js
@@ -6,17 +6,20 @@ import PostForm from '../form/PostForm';
 import PostList from '../list/PostList';
 import PostNotFound from '../not-found/PostNotFound';
 
+const rerender = Component =>
+  props => <Component { ...props } />;
+
 export default () => (
   <Switch>
-    <Route exact path="/" component={ PostList } />
+    <Route exact path="/" component={ rerender(PostList) } />
 
-    <Route exact path="/create" component={ PostForm } />
+    <Route exact path="/create" component={ rerender(PostForm) } />
 
     <Route exact path="/not-found" component={ PostNotFound } />
 
-    <Route exact path="/:category" component={ props => <PostList { ...props } /> } />
-    <Route exact path="/:category/:postId" component={ PostDetails } />
-    <Route exact path="/:category/:postId/edit" component={ PostForm } />
+    <Route exact path="/:category" component={ rerender(PostList) } />
+    <Route exact path="/:category/:postId" component={ rerender(PostDetails) } />
+    <Route exact path="/:category/:postId/edit" component={ rerender(PostForm) } />
 
     <Redirect from="*" to="/not-found" />
   </Switch>
